a987e816b96f32564031cd42f1c7aeeae50e0887
Alex Kravets <kravets.dev@gmail.com>
2019-06-10 22:48:27 -0400

    Added @Mock for test

--- demo-1/src/test/java/com/example/demo/EmployeeTest.java
+++ demo-1/src/test/java/com/example/demo/EmployeeTest.java
@@ -1,15 +1,25 @@
 package com.example.demo;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class EmployeeTest {
+	@Mock
+	Developer developerMock;
+	
 	@Test
 	public void testGetType(){
-		IEmployee developerMock = mock(Developer.class);
+		/**
+		 *  We can define our mock object like this, or we can remove this line and use 
+		 *  @Mock annotation as we did above
+		 */
+		//IEmployee developerMock = mock(Developer.class);
 		when(developerMock.getType()).thenReturn(IEmployee.Type.SALARY);
 		
 		Developer developer = new Developer();
