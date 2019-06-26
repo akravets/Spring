6aad143124f75db4ec13163a6a883005f5776af5
Alex Kravets <kravets.dev@gmail.com>
2019-06-26 16:13:43 -0400

    Added RowMapper

--- database/src/main/java/com/akravets/spring/database/repository/PersonDAO.java
+++ database/src/main/java/com/akravets/spring/database/repository/PersonDAO.java
@@ -1,5 +1,8 @@
 package com.akravets.spring.database.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 
 import com.akravets.spring.database.model.Person;
@@ -7,12 +10,28 @@ import com.akravets.spring.database.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PersonDAO {
     @Autowired
     JdbcTemplate template;
+
+    //We can use custom RowMapper to map fields from database to our POJO
+    class PersonRowMapper implements RowMapper<Person> {
+
+        @Override
+        public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Person person = new Person();
+            person.setId(rs.getInt("id");
+            person.setName(rs.getString("name"));
+            person.setLocation(rs.getString("location"));
+            person.setBirthday(rs.getTimestamp("birthday"));
+            return null;
+        }
+
+    }
 
     public List<Person> findAll() {
         // we can use Person class using BeanPropertyRowMapper because fields in Person class
